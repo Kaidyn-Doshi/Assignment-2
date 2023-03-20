680aa239c54cd4f975ed13e6cdc91a1d670927f1
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-20 10:39:58 -0400

    Update calculate-python.py

--- calculate-python.py
+++ calculate-python.py
@@ -1,2 +1,22 @@
 pi = 3.14159
 
+print('Please enter your diameter:')
+
+def diameter():
+  diameter = input()
+  return diameter
+
+def radius():
+  global calcRadius
+  calcRadius = diameter / 2
+  return calcRadius
+
+def area():
+  calcArea = pi * calcRadius ** calcRadius
+  return calcArea
+
+def circumference():
+  calcCirc = pi * diameter
+  return calcCirc
+
+print('For a circle with a diameter of' + diameter() 'units:', '\n','The radius would be:' + radius(), '\n', 'The area would be:' + area(), '\n', 'And the circumference would be:' circumference())
