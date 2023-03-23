d33a161ff4b69cd176ad0c5c3f105d38081a00a9
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-22 20:17:54 -0400

    Update php-calculator.php

--- php-calculator.php
+++ php-calculator.php
@@ -8,35 +8,38 @@
 
 
 <main class="mdl-layout__content">
-  <div class="page-content mdl-grid">
-    <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset mdl-shadow--2dp">
-      <form action="" method="GET" class="mdl-grid" autocomplete="off">
-        <div class="mdl-cell mdl-cell--12-col">
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-            <input class="mdl-textfield__input" type="number" id="diameter" name="diameter">
-            <label class="mdl-textfield__label" for="diameter">Diameter</label>
+    <div class="page-content mdl-grid">
+      <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset mdl-shadow--2dp">
+        <form action="" method="GET" class="mdl-grid" autocomplete="off">
+          <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="text" id="diameter" name="diameter" pattern="[0-9]+(\.[0-9]+)?" title="Invalid input">
+              <label class="mdl-textfield__label" for="diameter">Diameter</label>
+            </div>
           </div>
-        </div>
-        <div class="mdl-cell mdl-cell--12-col">
-          <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect mdl-cell--12-col">
-            Calculate
-          </button>
-        </div>
-      </form>
-        <?php
-        if(isset($_GET['diameter'])){
-            $dia = $_GET['diameter'];
-            $radius = $dia / 2;
-            $area = round(pi() * pow($radius, 2), 2);
-            $circumference = round(2 * pi() * $radius, 2);
-            echo "<div class='results'>";
-            echo "Diameter: $dia <br>";
-            echo "Radius: " . round($radius, 2) . "<br>";
-            echo "Area: $area <br>";
-            echo "Circumference: $circumference <br>";
-            echo "</div>";
-        }
-        ?>
+          <div class="mdl-cell mdl-cell--12-col">
+            <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect mdl-cell--12-col">
+              Calculate
+            </button>
+          </div>
+        </form>
+          <?php
+          if(isset($_GET['diameter']) && $_GET['diameter'] >= 0){
+              $dia = $_GET['diameter'];
+              $radius = $dia / 2;
+              $area = round(pi() * pow($radius, 2), 2);
+              $circumference = round(2 * pi() * $radius, 2);
+              echo "<div class='results'>";
+              echo "Diameter: $dia <br>";
+              echo "Radius: " . round($radius, 2) . "<br>";
+              echo "Area: $area <br>";
+              echo "Circumference: $circumference <br>";
+              echo "</div>";
+          } 
+          elseif(isset($_GET['diameter']) && $_GET['diameter'] < 0){
+              echo "<div class='results'>Please enter a non-negative integer only.</div>";
+          }
+          ?>
+      </div>
     </div>
-  </div>
-</main>
+  </main>
